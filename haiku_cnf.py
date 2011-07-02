87cd5d0c212e377cffa5d3e2a00c143fe751c6fb
Jrabbit <jackjrabbit+github@gmail.com>
2011-07-01 20:54:35 -0500

    attempt explicit comparison

--- haiku_cnf.py
+++ haiku_cnf.py
@@ -122,7 +122,7 @@ if __name__ == '__main__':
             #works
             print "This application is availible via `installoptionalpackage %s`" \
             % command
-        elif options['haikuports']:
+        elif options['haikuports'] == True:
             if command in db['haikuports']:
                 print "This application is availible via `haikuporter -i %s`" % command
         else:
