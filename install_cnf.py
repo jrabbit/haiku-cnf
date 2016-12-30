b468e9e1ae5eb12bd8416b85bcce28fb97931bd2
jrabbit <jackjrabbit@gmail.com>
2016-12-29 22:30:17 -0500

    bogus :poop: tests

--- install_cnf.py
+++ install_cnf.py
@@ -20,13 +20,19 @@ default_options="""{
 "spellcheck": false,
 "haikuports": false
 }\n"""
-home = os.environ['HOME']
-if not os.path.exists("%s/config/settings/command-not-found/options.json" % home):
-    profile = open("/etc/profile", "a")
-    profile.write(hacks)
-    profile.close()
-    os.mkdir("%s/config/settings/command-not-found/" % home)
-    options = open("%s/config/settings/command-not-found/options.json" % home, "w")
-    options.write(default_options)
-    options.close()
-os.system("install -m 755 haiku_cnf.py /boot/system/non-packaged/bin/command_not_found.py")
+
+def main():
+    home = os.environ['HOME']
+    if not os.path.exists("%s/config/settings/command-not-found/options.json" % home):
+        profile = open("/etc/profile", "a")
+        profile.write(hacks)
+        profile.close()
+        os.mkdir("%s/config/settings/command-not-found/" % home)
+        options = open("%s/config/settings/command-not-found/options.json" % home, "w")
+        options.write(default_options)
+        options.close()
+    os.system("install -m 755 haiku_cnf.py /boot/system/non-packaged/bin/command_not_found.py")
+
+
+if __name__ == '__main__':
+    main()
---
+++ test_install.py
@@ -0,0 +1,8 @@
+import unittest
+import mock
+
+from install_cnf import main
+
+class InstallerTest(unittest.TestCase):
+    def test_bogus(self):
+        pass
